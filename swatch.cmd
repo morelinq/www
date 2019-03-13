6b0d939339f8a30b1a56ed6aa95b8e2c3e241dee
Atif Aziz <code@raboof.com>
2019-03-13 11:00:55 +0100

    Fail swatch script if build fails

--- swatch.cmd
+++ swatch.cmd
@@ -1,5 +1,7 @@
 @echo off
 pushd "%~dp0"
-call build
+call build || goto :end
 start "dotnet watch build [%~dp0]" dotnet watch build
 start "mkdocs serve [%~dp0]" docker run --rm -it -p 8000:8000 -v "%~dp0:/docs" squidfunk/mkdocs-material
+:end
+exit /b %errorlevel%
